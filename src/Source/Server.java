af234c227883aa1dad77da2f398920c1db28b1d7
cstyan <callumstyan@gmail.com>
2014-11-02 23:47:39 -0800

    can you build please? WTF Travis

--- src/Source/Server.java
+++ src/Source/Server.java
@@ -17,7 +17,7 @@ import java.util.logging.Logger;
 
 public class Server {
 
-    // a unique ID for each connection
+    //a unique ID for each connection
     private static int uniqueId;
     // an ArrayList to keep the list of the Client
     private ArrayList<ClientThread> al;

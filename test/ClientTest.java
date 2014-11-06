9206f6dfa067b42cb52e1c734bd9b5d093c60c64
root <benjamin3726@hotmail.com>
2014-11-05 23:57:53 -0800

    Finish Test add user & edit user

--- test/ClientTest.java
+++ test/ClientTest.java
@@ -47,6 +47,8 @@ public class ClientTest {
     public void testNewUser() {
         Client c = new Client("", 1500);
 
+        System.out.println("******** TESTING ADDING A NEW USER ********");
+
         System.out.println("TEST: Using invalid username");
         assertEquals("Username is not valid", false, c.newUser("","",-1,-1));
 
@@ -56,7 +58,35 @@ public class ClientTest {
         System.out.println("TEST: Using Valid username, Valid Password, Invalid usertype");
         assertEquals("Username is not valid", false, c.newUser("Ben","BenPassword",-1,-1));
 
+        System.out.println("TEST: Using Valid username, Valid Password, Valid usertype, Invalid teamid");
+        assertEquals("Username is not valid", false, c.newUser("Ben","BenPassword",0,-1));
+
+        System.out.println("TEST: Using Valid username, Valid Password, Valid usertype, Valid teamid");
+        assertEquals("Username is not valid", true, c.newUser("Ben","BenPassword",0,7));
+
+        System.out.println("");
+
     }
 
 
+    @Test
+    public void testEdit() {
+        Client c = new Client("", 1500);
+
+        System.out.println("******** TESTING EDITING A USER ********");
+
+        System.out.println("TEST: Using invalid username");
+        assertEquals("Username is not valid", false, c.newUser("","",-1,-1));
+
+        System.out.println("TEST: Using Valid username, invalid Password");
+        assertEquals("Username is not valid", false, c.newUser("Ben","",-1,-1));
+
+        System.out.println("TEST: Using Valid username, Valid Password, Invalid usertype");
+        assertEquals("Username is not valid", false, c.newUser("Ben","BenPassword",-1,-1));
+
+        System.out.println("TEST: Using Valid username, Valid Password, Valid usertype");
+        assertEquals("Username is not valid", true, c.newUser("Ben","BenPassword",0,7));
+
+        System.out.println("");
+    }
 }
